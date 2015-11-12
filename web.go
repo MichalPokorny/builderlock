f559ae87c580976a34dc90a210bd6ac615436927
Michal Pokorný <pok@rny.cz>
2015-11-12 23:29:14 +0100

    First commit

--- web.go
+++ web.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"runtime"
+	"io/ioutil"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", statusResponse)
 	bind := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
 	fmt.Printf("listening on %s...", bind)
 	err := http.ListenAndServe(bind, nil)
@@ -17,6 +17,55 @@ func main() {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "hello, world from %s", runtime.Version())
+func getLockfilePath() string {
+	return os.Getenv("OPENSHIFT_DATA_DIR") + "/lock";
+}
+
+// Returns empty string if not locked.
+func getLocker() string {
+	path := getLockfilePath();
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// Create new lockfile (by releasing the lock).
+		releaseLock()
+	}
+
+	chars, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return string(chars)
+}
+
+func grabLock(locker string) {
+	if err := ioutil.WriteFile(getLockfilePath(), []byte(locker), 0); err != nil {
+		panic(err)
+	}
+}
+
+func releaseLock() {
+	if err := ioutil.WriteFile(getLockfilePath(), []byte(""), 0); err != nil {
+		panic(err);
+	}
+}
+
+func statusResponse(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "PUT":
+		if getLocker() != "" {
+			fmt.Fprintf(res, "cannot lock, sorry<br>")
+		} else {
+			grabLock(req.PostFormValue("locker"))
+		}
+	case "DELETE":
+		releaseLock()
+	}
+
+	locker := getLocker()
+	if locker == "" {
+		fmt.Fprintf(res, "unlocked right now<br>")
+		fmt.Fprintf(res, "<form action='/lock' method='put'><input type='text' name='locker' placeholder='My Glorious Name'><input type='submit' value='grab lock'></form>")
+	} else {
+		fmt.Fprintf(res, "<b>" + locker + "</b> has the lock since<br>")
+		fmt.Fprintf(res, "<form action='/lock' method='delete'><input type='submit' value='release lock'></form>")
+	}
 }
